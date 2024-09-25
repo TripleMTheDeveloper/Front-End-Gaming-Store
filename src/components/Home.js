b109edc18fa97e4d9727f891e06005a55063c7f2
Marcus Mdlalo <marcusmdlalo252@gmail.com>
2024-09-25 16:21:22 +0200

    Made changes to the product component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeHeader from './images/GamingHeader.png';
+import HomeHeader2 from './images/GamingHeader3.jpg';
 import { Card, Figure } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import FMCover from './images/FM24PC.jpeg';
@@ -16,28 +17,40 @@ import MKCover from './images/MKCover.jpg';
 import SFCover from './images/SFCover.jpeg';
 import GuiltyGear from './images/GuiltyGear.jpeg';
 import DBZCover from './images/DBZCover.jpeg';
+import GamingProduct from './images/GamingProduct.jpg';
+import GamingProduct2 from './images/GamingProduct2.png';
+import GamingProduct3 from './images/GamingProduct3.jpg';
+import GamingProduct4 from './images/GamingProduct4.webp';
+
 
 
 function Home() {
     const featuredData = [
-    { name: "Mortal Kombat", description: "Wresting entertainment simulator", price: 1250, image: MKCover },
-    { name: "Street Fighter", description: "Wresting entertainment simulator", price: 1150, image: SFCover },
-    { name: "Guilty Gear Strive", description: "Wresting entertainment simulator", price: 1250, image: GuiltyGear },
-    { name: "Dragon Ball Z Kakarot", description: "Wresting entertainment simulator", price: 799, image: DBZCover }
+    { name: "Mortal Kombat", description: "", price: 1250, image: MKCover },
+    { name: "Street Fighter", description: "", price: 1150, image: SFCover },
+    { name: "Guilty Gear Strive", description: "", price: 1250, image: GuiltyGear },
+    { name: "Dragon Ball Z Kakarot", description: "", price: 799, image: DBZCover }
      ];
 
     const newData = [
-      { name: "Football Manager 24", description: "football simulator", price: 649, image: FMCover },
-      { name: "Football Manager 24 ", description: "football simulator", price: 599, image: FMCover2 },
-      { name: "EA FC 24 LIMITED EDITION", description: "football game", price: 1499, image: EAFCCover2 },
-      { name: "EA FC 24", description: "The world's leading football game", price: 1000, image: EAFCCover }
+      { name: "Football Manager 24", description: "", price: 649, image: FMCover },
+      { name: "Football Manager 24 ", description: "", price: 599, image: FMCover2 },
+      { name: "EA FC 24 LIMITED EDITION", description: "", price: 1499, image: EAFCCover2 },
+      { name: "EA FC 24", description: "", price: 1000, image: EAFCCover }
       ];
 
     const oldData = [
-      { name: "F1 24", description: "Racing game", price: 625, image: F1Cover },
-      { name: "F1 Manager", description: "Racing simulator", price: 625, image: F1Cover2 },
-      { name: "NBA 2k24", description: "In honour of the mamber", price: 1000, image: NBA },
-      { name: "WWE 2k24", description: "Wresting entertainment simulator", price: 1250, image: WWE }
+      { name: "F1 24", description: "", price: 625, image: F1Cover },
+      { name: "F1 Manager", description: "", price: 625, image: F1Cover2 },
+      { name: "NBA 2k24", description: "", price: 1000, image: NBA },
+      { name: "WWE 2k24", description: "", price: 1250, image: WWE }
+      ];
+
+    const freeData = [
+      { name: "Apex Legends", description: "", price: 0, image: GamingProduct  },
+      { name: "Fortnite", description: "", price: 0, image: GamingProduct2  },
+      { name: "Valorant", description: "", price: 0, image: GamingProduct3  },
+      { name: "Counter-Strike", description: "", price: 0, image: GamingProduct4  }
       ];
   return (
     <div className='container'>
@@ -46,7 +59,7 @@ function Home() {
           <Link to="/products">
             <button className="shop-now-button">Shop Now</button>
           </Link>
-          <hr></hr>
+          <hr className='custom-hr'></hr>
           <div className="row g-5">
             <h className="section-header">Featured</h>
             {/* Map through the productData array and render a Card for each product */}
@@ -70,7 +83,7 @@ function Home() {
             ))}
           </div>
 
-          <hr></hr>
+          <hr className='custom-hr'></hr>
           <div className="row g-5">
             <h className="section-header">New Releases</h>
             {/* Map through the productData array and render a Card for each product */}
@@ -93,7 +106,35 @@ function Home() {
               </div>
             ))}
           </div>
-          <hr></hr>
+          <hr className='custom-hr'></hr>
+          <img className='home-header2' src={HomeHeader2} alt='HomeHeader2'/>
+          <Link to="/products">
+            <button className="shop-now-button2">Explore</button>
+          </Link>
+          <hr className='custom-hr'></hr>
+          <div className="row g-5">
+            <h className="section-header">FREE</h>
+            {/* Map through the productData array and render a Card for each product */}
+            {freeData.map((product, index) => (
+              <div key={index} className="col-6 col-md-3 mb-3">
+                <Card className='product-card'>
+                  <Figure>
+                    <Figure.Image
+                      src={product.image}
+                      alt={product.name}
+                      className="figure-img"
+                    />
+                  </Figure>
+                  <Card.Body className='card-body'>
+                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Text>Price: R{product.price.toFixed(2)}</Card.Text>
+                    {/* Button to add product to cart */}
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
+          </div>
+          <hr className='custom-hr'></hr>
           <div className="row g-5">
             <h className="section-header">On Special</h>
             {/* Map through the productData array and render a Card for each product */}
