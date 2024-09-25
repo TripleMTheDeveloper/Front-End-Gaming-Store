64a5ddfb4f568630533e10634362ac4ce3134687
Marcus Mdlalo <marcusmdlalo252@gmail.com>
2024-09-25 16:21:18 +0200

    Made changes to the product page

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { Card, Button, Figure } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Product.css';
-import { CartContext, TotalPrice } from './Cart';
+import { CartContext } from './Cart';
 import FMCover from './images/FM24PC.jpeg';
 import FMCover2 from './images/FM24Console.jpeg';
 import EAFCCover2 from './images/EAFC2.jpeg';
@@ -16,6 +16,7 @@ import SFCover from './images/SFCover.jpeg';
 import GuiltyGear from './images/GuiltyGear.jpeg';
 import DBZCover from './images/DBZCover.jpeg';
 import Checkout from './Checkout';
+import ProductHeader from './images/GamingHeader5.jpg';
 
 function Products() {
   // Get the addToCart function from the CartContext using useContext hook
@@ -25,7 +26,7 @@ function Products() {
   const [showCheckout, setShowCheckout] = useState(false);
 
   // Array of product data containing name, description, price, and image for each product
-  const productData = [
+  const sportsData = [
     { name: "Football Manager 24", description: "football simulator", price: 649, image: FMCover },
     { name: "Football Manager 24 ", description: "football simulator", price: 599, image: FMCover2 },
     { name: "EA FC 24 LIMITED EDITION", description: "football game", price: 1499, image: EAFCCover2 },
@@ -34,6 +35,9 @@ function Products() {
     { name: "F1 Manager", description: "Racing simulator", price: 625, image: F1Cover2 },
     { name: "NBA 2k24", description: "In honour of the mamber", price: 1000, image: NBA },
     { name: "WWE 2k24", description: "Wresting entertainment simulator", price: 1250, image: WWE },
+  ];
+
+  const fightingData = [
     { name: "Mortal Kombat", description: "Wresting entertainment simulator", price: 1250, image: MKCover },
     { name: "Street Fighter", description: "Wresting entertainment simulator", price: 1150, image: SFCover },
     { name: "Guilty Gear Strive", description: "Wresting entertainment simulator", price: 1250, image: GuiltyGear },
@@ -57,15 +61,17 @@ function Products() {
 
   return (
     <div className='container-product'>
+      <img className='product-header' src={ProductHeader} alt='gaming header'/>
       {showCheckout ? (
         // Show the Checkout component if showCheckout state is true
         <Checkout onPaymentSuccess={handlePaymentSuccess} onBack={handleBackToProducts} />
       ) : (
         <>
-          <h2>Products</h2>
+          x
           <div className="row g-5">
+            <h className="section-header">Sports</h>
             {/* Map through the productData array and render a Card for each product */}
-            {productData.map((product, index) => (
+            {sportsData.map((product, index) => (
               <div key={index} className="col-6 col-md-3 mb-3">
                 <Card className='product-card'>
                   <Figure>
@@ -77,7 +83,6 @@ function Products() {
                   </Figure>
                   <Card.Body className='card-body' >
                     <Card.Title>{product.name}</Card.Title>
-                    <Card.Text>{product.description}</Card.Text>
                     <Card.Text>Price: R{product.price.toFixed(2)}</Card.Text>
                     {/* Button to add product to cart */}
                     <Button className='btn-custom' onClick={() => addToCart(product)}>Add to Cart</Button>
@@ -86,11 +91,31 @@ function Products() {
               </div>
             ))}
           </div>
-          <div className='total-price'>
-            {/* Component to display the total price */}
-            <TotalPrice />
+          <hr className='custom-hr'></hr>
+          <div className="row g-5">
+            <h className="section-header">Fighting</h>
+            {/* Map through the productData array and render a Card for each product */}
+            {fightingData.map((product, index) => (
+              <div key={index} className="col-6 col-md-3 mb-3">
+                <Card className='product-card'>
+                  <Figure>
+                    <Figure.Image
+                      src={product.image}
+                      alt={product.name}  
+                      className="figure-img "
+                    />
+                  </Figure>
+                  <Card.Body className='card-body' >
+                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Text>Price: R{product.price.toFixed(2)}</Card.Text>
+                    {/* Button to add product to cart */}
+                    <Button className='btn-custom' onClick={() => addToCart(product)}>Add to Cart</Button>
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
           </div>
-          {/* Button to proceed to checkout */}
+          <hr className='custom-hr'></hr>
           <Button className='btn-custom1' onClick={handleCheckout}>Proceed to Checkout</Button>
         </>
       )}
